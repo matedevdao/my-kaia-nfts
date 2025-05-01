1d1d59e41616ac8a1a3ccd26fd09501ae8e56551
yjgaia <yjdev88@gmail.com>
2025-05-01 14:08:09 +0900

    Implement code changes to enhance functionality and improve performance

--- dist/views/HomeView.js
+++ dist/views/HomeView.js
@@ -1,5 +1,6 @@
 import { BodyNode, el, Router, View } from "@commonmodule/app";
 import { KaiaWalletSessionManager } from "kaia-wallet-module";
+import { HoldingNFTList } from "matedevdao-common";
 export default class HomeView extends View {
     constructor() {
         super();
@@ -8,6 +9,7 @@ export default class HomeView extends View {
             Router.goWithoutHistory("/connect-required");
         }
         else {
+            this.container.append(new HoldingNFTList("0x8033cEB86c71EbBF575fF7015FcB8F1689d90aC1"));
         }
     }
 }
