1ce3709d89bf836a81e15f25adcff912f5637f92
yjgaia <yjdev88@gmail.com>
2025-05-02 20:47:49 +0900

    Refactor code structure for improved readability and maintainability

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,5 @@
 import { Router, SPAInitializer } from "@commonmodule/app";
+import { KaiaWalletLoginConfig } from "kaia-wallet-login-module";
 import { MDDModuleConfig } from "matedevdao-common";
 import HomeView from "./views/HomeView.js";
 import Layout from "./views/Layout.js";
@@ -11,8 +12,12 @@ if (location.pathname.includes("/my-kaia-nfts")) {
   Router.prefix = "/my-kaia-nfts";
 }
 
-MDDModuleConfig.init({
+MDDModuleConfig.init();
+
+KaiaWalletLoginConfig.init({
   appName: "My Kaia NFTs",
+  apiBaseURL: "https://api.matedevdao.workers.dev",
+  walletConnectProjectId: "647df3b69553d6f1261ea1482bd90d4a",
 });
 
 Router
