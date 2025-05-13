395d470aa917286ebd89dc0409edabe87777e00a
yjgaia <yjdev88@gmail.com>
2025-05-13 14:55:15 +0900

    Add transfer functionality and prompt dialog to NFTView for improved user interaction

--- app/views/NFTView.ts
+++ app/views/NFTView.ts
@@ -1,5 +1,9 @@
 import { el, Router, View } from "@commonmodule/app";
-import { AppCompConfig, Button } from "@commonmodule/app-components";
+import {
+  AppCompConfig,
+  Button,
+  PromptDialog,
+} from "@commonmodule/app-components";
 import { KaiaWalletLoginManager } from "kaia-wallet-login-module";
 import {
   MDDModuleConfig,
@@ -7,6 +11,17 @@ import {
   NFTDisplay,
 } from "matedevdao-common";
 import Layout from "./Layout.js";
+
+const collectionAddresses: Record<string, `0x${string}`> = {
+  "dogesoundclub-mates": "0xE47E90C58F8336A2f24Bcd9bCB530e2e02E1E8ae",
+  "dogesoundclub-e-mates": "0x2B303fd0082E4B51e5A6C602F45545204bbbB4DC",
+  "dogesoundclub-biased-mates": "0xDeDd727ab86bce5D416F9163B2448860BbDE86d4",
+  "sigor-sparrows": "0x7340a44AbD05280591377345d21792Cdc916A388",
+  "sigor-housedeeds": "0x455Ee7dD1fc5722A7882aD6B7B8c075655B8005B",
+  "kingcrowndao-kongz": "0xF967431fb8F5B4767567854dE5448D2EdC21a482",
+  "kingcrowndao-pixel-kongz": "0x81b5C41Bac33ea696D9684D9aFdB6cd9f6Ee5CFF",
+  "babyping": "0x595b299Db9d83279d20aC37A85D36489987d7660",
+};
 
 export default class NFTView extends View {
   public changeData(
@@ -26,10 +41,47 @@ export default class NFTView extends View {
     this.container.append(
       el(
         "header",
+        nftData.holder === KaiaWalletLoginManager.getLoggedInAddress()
+          ? new Button({
+            title: "전송하기",
+            onPress: async () => {
+              await new PromptDialog({
+                title: "전송하기",
+                message: "전송할 주소를 입력하세요",
+                placeholder: "0x...",
+                confirmButtonTitle: "전송하기",
+                cancelButtonTitle: "취소",
+                onConfirm: async (address) => {
+                  await KaiaWalletLoginManager.writeContract({
+                    address: collectionAddresses[nftData.collection],
+                    chainId: 8_217,
+                    functionName: "transferFrom",
+                    args: [
+                      KaiaWalletLoginManager.getLoggedInAddress(),
+                      address,
+                      nftData.id,
+                    ],
+                    abi: [
+                      {
+                        name: "transferFrom",
+                        type: "function",
+                        inputs: [
+                          { name: "from", type: "address" },
+                          { name: "to", type: "address" },
+                          { name: "tokenId", type: "uint256" },
+                        ],
+                      },
+                    ],
+                  });
+                },
+              }).waitForConfirmation();
+            },
+          })
+          : undefined,
         nftData.holder === KaiaWalletLoginManager.getLoggedInAddress() &&
           MDDModuleConfig.isEditableNFTCollection(nftData.collection)
           ? new Button({
-            title: "Edit",
+            title: "정보 수정",
             onPress: () =>
               Router.go(`/${nftData.collection}/${nftData.id}/edit`, nftData),
           })
